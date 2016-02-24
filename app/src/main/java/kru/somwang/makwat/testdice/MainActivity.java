8bcb37eacde7e1825d298283513f915db6beb8d4
Makwat <makwat.fish@gmail.com>
2016-02-24 10:41:23 +0700

    bindWidget fin

--- app/src/main/java/kru/somwang/makwat/testdice/MainActivity.java
+++ app/src/main/java/kru/somwang/makwat/testdice/MainActivity.java
@@ -2,12 +2,29 @@ package kru.somwang.makwat.testdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageDice;
+    private Button btnback, btnran, btnnext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
     }
+
+    private void bindWidget() {
+        imageDice = (ImageView) findViewById(R.id.imageDice);
+        btnback = (Button) findViewById(R.id.btnback);
+        btnran = (Button) findViewById(R.id.btnran);
+        btnnext = (Button) findViewById(R.id.btnnext);
+
+    }
+
+
 }
